e558eef071efc02ceac4af1d421cc21d97ec97c4
oliverwood93 <oliverwood1993@hotmail.co.uk>
2019-02-11 17:25:12 +0000

    setting up api

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const app = require('express')()
-const apiRouter = require('./routes/api-router')
-const bodyParser = require('body-parser')
+const app = require('express')();
+const bodyParser = require('body-parser');
+const apiRouter = require('./routes/api-router');
 
 app.use(bodyParser.json());
 
 app.use('/api', apiRouter);
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: 'SERVER_ERROR_500' });
+});
 
-app.use(err, req, res, next) => {
-  res.status(500).send({msg: 'SERVER_ERROR_500'})
-}
-
-module.exports = app
+module.exports = app;
